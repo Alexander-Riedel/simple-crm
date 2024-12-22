508d9d1c8a316117147ae793bfd8c95057df4689
Alexander Riedel <alexander.riedel@riedelmedia.de>
2024-12-22 21:48:53 +0100

    add user table

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,23 +5,40 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatNativeDateModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatNativeDateModule,
+    MatTableModule
+  ],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
+  dataSource = new MatTableDataSource<any>();
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'street', 'zipCode', 'city'];
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private firestore: Firestore) { }
 
-  openDialog() {
-    const buttonElement = document.activeElement as HTMLElement; // Get the currently focused element
-    buttonElement.blur(); // Remove focus from the button
-
-    this.dialog.open(DialogAddUserComponent);
+  ngOnInit(): void {
+    const usersCollection = collection(this.firestore, 'users');
+    collectionData(usersCollection).subscribe((changes: any[]) => {
+      console.log('Received changes from DB', changes);
+      this.dataSource.data = changes;
+    });
   }
 
+  openDialog() {
+    const buttonElement = document.activeElement as HTMLElement;
+    buttonElement.blur();
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
